a3842b7c7ff9eee4a1a008f45f21ea8e74cd6b5a
jencekade <gaephuoc@gmail.com>
2017-11-10 09:26:35 +0000

    automatically backed up by learn

--- question.js
+++ question.js
@@ -13,10 +13,17 @@ class Question {
     return this._All;
   }
 
+
+/*so what this is asking is that giving Find(number),
+we want to get the numbered item inside the Array
+and not to find something inside that matches the ID 
   static Find(id) {
     return this.All().find(function(ask) {
       return ask.id == id;
     });
+  }*/
+  static Find(id){
+    return this.All()[id -1];
   }
 
 }
