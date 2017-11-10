eee1d814550b2945b2f4f5acf1d72cba7e131c60
jencekade <gaephuoc@gmail.com>
2017-11-10 09:05:09 +0000

    Done.

--- question.js
+++ question.js
@@ -1,1 +1,24 @@
 'use strict';
+class Question {
+  constructor (question) {
+    this.question = question;
+    this.save();
+    //Question._All.push(this);
+  }
+
+  save() {
+    this.constructor._All.push(this);
+  }
+
+  static All() {
+    return this._All;
+  }
+
+  static Find(id) {
+    return this.All().find(function(id) {
+      return Question._All == id;
+    });
+  }
+
+}
+Question._All = [];
